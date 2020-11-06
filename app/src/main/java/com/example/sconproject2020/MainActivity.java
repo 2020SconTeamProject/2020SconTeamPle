975cfa53d04788294bda45a1c9bd6e5f1c3cf756
gyu6172 <gyu6172@gmail.com>
2020-11-06 09:22:10 +0900

    bottom navigation add

--- app/src/main/java/com/example/sconproject2020/MainActivity.java
+++ app/src/main/java/com/example/sconproject2020/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.sconproject2020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,25 +18,46 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Fragment> fragments = new ArrayList<>();
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+
+
+    BottomNavigationView bottomNavigationView;
+    private HomeFragment homeFragment = new HomeFragment();
+    private CalendarFragment calendarFragment = new CalendarFragment();
+    private MapFragment mapFragment = new MapFragment();
+    private SettingFragment settingFragment = new SettingFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HomeFragment homeFragment = new HomeFragment();
-        CalendarFragment calendarFragment = new CalendarFragment();
-        MapFragment mapFragment = new MapFragment();
-        SettingFragment settingFragment = new SettingFragment();
-        fragments.add(homeFragment);
-        fragments.add(calendarFragment);
-        fragments.add(mapFragment);
-        fragments.add(settingFragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_frame, homeFragment).commitAllowingStateLoss();
 
-        BottomNavigationView navigationView =findViewById(R.id.nav_host_view);
+        bottomNavigationView =findViewById(R.id.main_bottomView);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch(item.getItemId()){
+                    case R.id.menuitem_home:
+                        transaction.replace(R.id.main_frame, homeFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.menuitem_calendar:
+                        transaction.replace(R.id.main_frame, calendarFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.menuitem_map:
+                        transaction.replace(R.id.main_frame, mapFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.menuitem_setting:
+                        transaction.replace(R.id.main_frame, settingFragment).commitAllowingStateLoss();
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 }
