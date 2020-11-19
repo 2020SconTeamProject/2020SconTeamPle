b8cce5b01cfb9329e2ff9f350828fb47046cb1b8
gyu6172 <gyu6172@gmail.com>
2020-11-20 08:56:26 +0900

    HomeFragment RecyclerView add

--- app/src/main/java/com/example/sconproject2020/Home/HomeFragment.java
+++ app/src/main/java/com/example/sconproject2020/Home/HomeFragment.java
@@ -3,14 +3,24 @@ package com.example.sconproject2020.Home;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.sconproject2020.Calendar.CalendarRecyclerItem;
 import com.example.sconproject2020.R;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+
+    TextView nowTodoTextView;
+    RecyclerView recyclerView;
+    ArrayList<HomeRecyclerItem> datas = new ArrayList<>();
 
     public HomeFragment() {
 
@@ -20,6 +30,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        nowTodoTextView = view.findViewById(R.id.nowTodoTv);
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        HomeRecyclerAdapter adapter = new HomeRecyclerAdapter(datas);
+        recyclerView.setAdapter(adapter);
+
+        for(int i=0;i<5;i++){
+            HomeRecyclerItem item = new HomeRecyclerItem("학교보내기", "8:00", false);
+            datas.add(item);
+        }
+
+        adapter.notifyDataSetChanged();
 
         return view;
     }
